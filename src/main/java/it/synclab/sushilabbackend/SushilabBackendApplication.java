507169e9510097feb6b4acb631d5e8c625a3cafc
contug <66022609+contug@users.noreply.github.com>
2022-05-11 10:47:20 +0200

    initial commit

---
+++ src/main/java/it/synclab/sushilabbackend/SushilabBackendApplication.java
@@ -0,0 +1,13 @@
+package it.synclab.sushilabbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SushilabBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SushilabBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/synclab/sushilabbackend/SushilabBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.synclab.sushilabbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SushilabBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
